bd6d6e13835eec48a4fd6992085dc616dda94ec5
Zackptg5 <zackptg5@comcast.net>
2019-01-07 21:54:17 -0500

    update

--- common/uninstall.sh
+++
@@ -1,7 +0,0 @@
-if ! $MAGISK || $SYSOVERRIDE; then
-  # Patch boot img if not using root solution that supports boot scripts
-  ROOTTYPE="other root/rootless"
-  POSTFSDATA=false
-  LATESTARTSERVICE=false
-  RAMDISK=true
-fi
